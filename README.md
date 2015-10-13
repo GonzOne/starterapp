12864d988ad9e64ccce8947697536cf85a11b095
Manuel Gonzale <design@stheory.com>
2015-10-13 16:26:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+## Stheory Starter App
+
+A simple front-end template that helps you build a fast, modern mobile web app.
+
+## Dependencies
+
+There are two tool sets you need to install on your machine before you can build this starter app: NodeJS, NPM
+
+## Installation
+
+(1) Install Node
+
+    If you aren’t sure if you have NodeJS and NPM, you can check by opening terminal (a command prompt) and running :
+
+    node -v.
+
+
+    If Node responds, check the version matches the current version on <a href="http://nodejs.org">NodeJS.org.</a>
+
+    If you don’t get a response or have an old version then go to NodeJS.org and click on the big green Install button.
+    NPM will be installed with NodeJS automatically.
+
+(2) Install the starter app
+
+    Download the zip rename the folder (your-project-name) and put it somewhere relevant on your machine.
+
+(3) Install dependencies
+
+    Next, you need to install the dependencies for the Stheory Starter App.
+    Open terminal (command prompt), change directory into your project folder and run the following npm install scripts.
+
+    cd your-project-name
+    npm install
+    npm install gulp -g
+
+
+That’s it! You now have everything that’s needed to use the starter app, happy coding.
+
+
+## Gulp Tasks
+
+gulp serve
+
+The task starts a local HTTP server so you can view your site in a browser, why developing.
+
+gulp serve:dist
+
+The gulp serve:dist command builds a production version of the app, it starts a server, and opens the browser for you, it’s a reliable way of testing the app before deploying it.
---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+
+
+// Lint JavaScript
+gulp.task('jshint', function () {
+    return gulp.src('app/scripts/*.js')
+        .pipe(reload({stream: true, once: true}))
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+
+// Minify and copy all JavaScript (except vendor scripts)
+gulp.task('scripts', function() {
+  gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**'])
+    .pipe($.uglify({
+        preserveComments: 'some',
+        mangle: false,
+        compress: true,
+        beautify: true
+    }))
+    .pipe($.rename('main.min.js'))
+    .pipe(gulp.dest('dist/scripts'));
+
+  // Copy vendor files
+  gulp.src('app/scripts/vendor/**')
+    .pipe(gulp.dest('dist/scripts/vendor'));
+});
+
+
+// Copy all static assets
+gulp.task('copy', function() {
+
+    //Css
+    gulp.src('app/styles/**/*')
+        .pipe(gulp.dest('dist/styles'))
+        .pipe($.size({title: 'css'}));
+
+    //Images   
+    gulp.src('app/images/**/*')
+        // Optimize images
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size({title: 'images'}));
+
+    //Fonts
+    gulp.src(['app/fonts/**'])
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe($.size({title: 'fonts'}));
+});
+
+gulp.task('html',function(){
+    var assets = $.useref.assets({searchPath: '{.tmp,app}'});
+    return gulp.src('app/**/*.html')
+    .pipe(assets)
+        .pipe($.if('*.css', $.uncss({
+            html: [
+                'app/index.html',
+                'app/contact.html'
+            ]
+        })))
+        .pipe($.if('*.css', $.csso()))
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe($.if('*.html', $.minifyHtml()))
+        .pipe(gulp.dest('dist'))
+        .pipe($.size({title: 'html'}));
+});
+
+// Clean output directory
+gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
+
+
+gulp.task('serve', function () {
+  browserSync({
+    notify: false,
+    logPrefix: 'WSK',
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: ['.tmp', 'app']
+  });
+
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/styles/**/*.css'], reload);
+  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+  gulp.watch(['app/images/**/*'], reload);
+  gulp.watch(['app/fonts/**/*'], reload);
+});
+
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['default'], function () {
+    browserSync({
+        notify: false,
+        logPrefix: 'WSK',
+        // Run as an https by uncommenting 'https: true'
+        // Note: this uses an unsigned certificate which on first access
+        //       will present a certificate warning in the browser.
+        // https: true,
+        server: 'dist'
+    });
+});
+
+// Build production files, the default task
+gulp.task('default', ['clean'], function (cb) {
+    runSequence('jshint', ['scripts','html','copy'], cb);
+});
